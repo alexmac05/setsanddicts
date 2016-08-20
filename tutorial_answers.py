b45c700fdbe448077baa37c52c75c933dc7c5ef9
Alex McFerron <alexmac2017@gmail.com>
2016-08-19 20:46:09 -0700

    exercise 7

--- tutorial_answers.py
+++ tutorial_answers.py
@@ -80,7 +80,27 @@
 # Excercise 7 - very simple hash table for illustration
 #KEY AND VALUE
 
-# Exercise 8 -
+# Exercise 8 - Illustrate the difference between a hash table and a set. (review dictionary )
+#Sets are just hashtables without values or dictionaries without values. Lists are resizable arrays that track
+#What can go in a list can be unhashable
+
+#Example of something that is unhashable - another list. lists are mutable. if a datatype is mutable it is not hashable
+#Because you can't reduce it to a unique value because it might change and the hash funciton would change.
 
 
+#excecise X - Dedup the list from the beginning using a set
+
+#myList = ['c', 'd', 'c', 'a', 'b', 'c', 'a', 'z', 'd', 'e', 'f', 'f', 'g']
+#mySet = set(myList) #Because it is using a hash funciton, this is O(n)
+#print(mySet)
+
+
+#Excercise X
+#myList = ['c', 'd', 'c', 'a', 'b', 'c', 'a', 'z', 'd', 'e', 'f', 'f', 'g']
+#myOtherList = ['c', 'c', 'a', 'z', 'p', 'q', ]
+#myListIsNowASet = set(myList)
+#myOtherListIsNowASet = set(myOtherList)
+#intersaction = myListIsNowASet&myOtherListIsNowASet
+#print(intersaction)
+#https://docs.python.org/2/library/stdtypes.html#set
 
