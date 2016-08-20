1db842197432a4c780858d253a68aaaafa5c99f8
Alex McFerron <alexmac2017@gmail.com>
2016-08-19 19:58:40 -0700

    question 4

--- tutorial_answers.py
+++ tutorial_answers.py
@@ -3,5 +3,53 @@
 #print("Hello world!")
 
 
-#Underlying hash tables
+# Question 2: Get rid of all of the duplicates in this list.
+#Think about the complexity of your solution.
+# Complexity cheetsheet http://bigocheatsheet.com/
+# You will find it is O(n^2), please make sure you can explain why as an exercise
+#Cool link https://www.peterbe.com/plog/uniqifiers-benchmark
 
+#myList = ['c', 'd', 'c', 'a', 'b', 'c', 'a', 'z', 'd', 'e', 'f', 'f', 'g']
+
+
+#ANWSER
+#newList = []
+#for i in myList:
+#    if i not in newList:
+#        newList.append(i)
+
+#print(newList)
+
+
+# Exercise 3: Build an address book only using lists (the lists can be lists of tuples of objects)
+
+#Answer
+#Lists of tuples
+#phonebook = [
+#    ("John Doe", "555-555-5555"),
+#    ("Albert Einstein", "212-555-5555"),
+#]
+#print(phonebook)
+
+#phonebookNames = ['John Doe', 'Albert Einstein']
+#phonebookNumbers = ['555-555-5555', '212-555-5555']
+#print(phonebookNames[0])
+#print(phonebookNumbers[0])
+
+
+# Exercise 4 : Build a look up function for your address book
+
+#Answer
+#phonebook = [
+#    ("John Doe", "555-555-5555"),
+#    ("Albert Einstein", "212-555-5555"),
+#]
+#print(phonebook)
+
+#def find_phonenumber(phonebook, name):
+#    for n, p in phonebook:
+#        if n == name:
+#            return p
+#        return None
+
+#print "John Doe's phone number is", find_phonenumber(phonebook, "John Doe")
